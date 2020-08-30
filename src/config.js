0185612041ce89deaf24e8ea9566e3aed1b9de0f
Jordan Henn <jordanwalkerhenn@gmail.com>
2020-08-29 18:37:20 -0700

    revising api endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  API_ENDPOINT: 'https://powerful-headland-70919.herokuapp.com/api'
+  API_ENDPOINT: 'postgres://erojnicfiqvbsf:e4c39afdc18ca85bbf14702aa4af86b391f5edaccb8f16180a9b73339b1ac1dc@ec2-54-243-67-199.compute-1.amazonaws.com:5432/dscg850p96f83/api'
 }
